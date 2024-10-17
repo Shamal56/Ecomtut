6feb9152d0c0604bea85e9f7f53e8ba2c72c97c4
Shamal Jayawardena <shamal.20230937@iit.ac.lk>
2024-10-17 22:00:59 +0530

    v 1.3

--- src/components/Square/Square.jsx
+++ src/components/Square/Square.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 
-import Pic1 from "../../assets/2.jpg";
-import Pic2 from "../../assets/Ghost.jpg";
-import Pic3 from "../../assets/188720.png";
+import Pic1 from "../../assets/Men.png";
+import Pic2 from "../../assets/Women.png";
+import Pic3 from "../../assets/Sale.png";
 import Slider from "react-slick";
 
 const ImageList = [
@@ -45,9 +45,9 @@ const Square = () => {
   }
 
   return (
-    <div className="relative overflow-hidden min-h-[550px] sm:min-[650px] bg-gray-100
+    <div className="relative overflow-hidden min-h-[600px] sm:min-[650px] bg-gray-100
      flex justify-center items-center dark:bg-gray-800 dark:text-white duration-200">
-      <div className="w-[700px] h-[700px] bg-primary/50 absolute -top-1/2 right-0 rounded-3xl rotate-45 -z-9">
+      <div className="w-[700px] h-[700px] bg-primary/50 absolute -top-[320px] right-0 rounded-3xl rotate-45 -z-9">
       
       </div>
       <div className="container pb-8 sm:pb-0">
@@ -57,10 +57,20 @@ const Square = () => {
               <div className="grid grid-cols-1 sm:grid-cols-2">
                 {/* text content section */}
                 <div className="flex flex-col justify-center gap-4 pt-12 sm:pt-0 text-center sm:text-left
-                order-2 sm:order-1 relative z-10">
-                  <h1 className="text-5xl sm:text-6xl lg:text-7xl font-bold">{data.title}</h1>
-                  <p className="text-sm">{data.description}</p>
-                  <div>
+                order-2 sm:order-1 relative z-10 ml-[10px]">
+                  <h1 data-aos='zoom-out'
+                      data-aos-once="true"
+                      data-aos-duration="500"
+                      className="text-5xl sm:text-6xl lg:text-7xl font-bold">{data.title}</h1>
+                  <p
+                    data-aos='fade-up'
+                    data-aos-duration="500"
+                    data-aos-delay="100" 
+                    className="text-sm">{data.description}</p>
+                  <div data-aos='fade-up'
+                    data-aos-duration="500"
+                    data-aos-delay="300"
+                  >
                     <button className="bg-gradient-to-r from-primary to-secondary hover:scale-105
                     duration-200 text-white py-2 px-4 rounded-full">
                       Order Now
@@ -69,8 +79,11 @@ const Square = () => {
                 </div>
                 {/* Image section */}
                 <div className="order-1 sm:order-2">
-                  <div className="relative z-10">
-                    <img src={data.img} alt="#" className="w-[300px] h-[300px] sm:h-[450px] sm:w-[450px] sm:scale-125 object-contain mx-auto"/>
+                  <div data-aos='fade-up'
+                      data-aos-once="true"
+                      data-aos-delay={data.aosDelay} 
+                      className="relative z-10">
+                    <img src={data.img} alt="#" className="w-[300px] h-[300px] sm:h-[450px] sm:w-[450px] sm:scale-105 lg:scale-120 object-contain mx-auto"/>
                   </div>
                 </div>
               </div>
