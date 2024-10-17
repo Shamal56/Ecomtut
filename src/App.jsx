8d518217ac1d78b383f0279acddc1ae351c0791d
Shamal Jayawardena <shamal.20230937@iit.ac.lk>
2024-10-17 22:00:33 +0530

    v 1.2

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,30 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
 import Square from './components/Square/Square';
+import Products from './components/products/Products';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import TopProducts from './components/TopProducts/TopProducts';
 
 
 const App = () => {
+
+  React.useEffect(() => {
+    AOS.init({
+      offset: 100,
+      duration: 200,
+      easing: 'ease-in-sine',
+      delay: 100,
+    });
+    AOS.refresh();
+  }, []);
+
   return (
     <div>
       <Navbar/>
       <Square/>
+      <Products/>
+      <TopProducts/>
     </div>
   )
 }
