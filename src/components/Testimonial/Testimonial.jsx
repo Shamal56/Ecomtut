1f5660a1fd39b416c8416311d28bd64edad28fcf
Shamal Jayawardena <shamal.20230937@iit.ac.lk>
2024-10-18 12:12:49 +0530

    v 3

--- src/components/Testimonial/Testimonial.jsx
+++ src/components/Testimonial/Testimonial.jsx
@@ -91,7 +91,7 @@ const Testimonial = () => {
                 <div
                   key={data.id}
                   className="flex flex-col gap-4
-              shadow-lg py-8 px-6 mx-4 rounded-xl dark:bg-gray-800
+              shadow-lg py-8 px-6 mx-4 rounded-xl dark:bg-gray-400
               bg-primary/10 relative"
                 >
                   <div className="mb-4">
