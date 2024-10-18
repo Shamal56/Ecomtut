3f35ef3067dd037d3eae4d553c976745d142e94c
Shamal Jayawardena <shamal.20230937@iit.ac.lk>
2024-10-18 11:06:12 +0530

    v 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
-import React from 'react'
-import Navbar from './components/Navbar/Navbar'
-import Square from './components/Square/Square';
-import Products from './components/products/Products';
-import AOS from 'aos';
-import 'aos/dist/aos.css';
-import TopProducts from './components/TopProducts/TopProducts';
-
+import React from "react";
+import Navbar from "./components/Navbar/Navbar";
+import Square from "./components/Square/Square";
+import Products from "./components/products/Products";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import TopProducts from "./components/TopProducts/TopProducts";
+import Banner from "./components/Banner/Banner";
+import Subscribe from "./components/Subscribe/Subscribe";
+import Testimonial from "./components/Testimonial/Testimonial";
+import Footer from "./components/Footer/Footer";
 
 const App = () => {
-
   React.useEffect(() => {
     AOS.init({
       offset: 100,
       duration: 200,
-      easing: 'ease-in-sine',
+      easing: "ease-in-sine",
       delay: 100,
     });
     AOS.refresh();
@@ -21,12 +23,17 @@ const App = () => {
 
   return (
     <div>
-      <Navbar/>
-      <Square/>
-      <Products/>
-      <TopProducts/>
+      <Navbar />
+      <Square />
+      <Products />
+      <TopProducts />
+      <Banner />
+      <Subscribe />
+      <Products />
+      <Testimonial />
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
 export default App;
